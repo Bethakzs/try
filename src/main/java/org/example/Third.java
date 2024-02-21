1de2cc7e5018d7795e8662ac925b257f2bf4fe41
Roman Levkiv <roman.levkiv.ri.2023@lpnu.ua>
2024-02-21 19:22:32 +0200

    init commit

---
+++ src/main/java/org/example/Third.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Third extends second{
+    public Third(int age, String name) {
+        super(age, name);
+    }
+}
---
+++ src/main/java/org/example/second.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class second {
+    private int age;
+    private String name;
+
+    public second (int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public void printName() {
+        System.out.println(name);
+    }
+}
